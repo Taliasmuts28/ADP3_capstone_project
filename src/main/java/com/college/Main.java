040d626068a46e8c746c12a53d55c4f0ed525435
TT Smuts <221126082@mycput.ac.za>
2025-03-12 12:59:05 +0200

    Update Main.java

--- src/main/java/com/college/Main.java
+++ src/main/java/com/college/Main.java
@@ -11,7 +11,7 @@ Members
 Name and surname | Student id | GitHub profile                |
 ---------------------------------------------------------------
 Ammaar Swartland | 230159478  | https://github.com/ammaar0x01 |
-Ammaar Swartland | 230159478  | https://github.com/ammaar0x01 |
+Talia Smuts      | 221116082  | https://github.com/taliasmuts28 |
 Ammaar Swartland | 230159478  | https://github.com/ammaar0x01 |
 Ammaar Swartland | 230159478  | https://github.com/ammaar0x01 |
 Ammaar Swartland | 230159478  | https://github.com/ammaar0x01 |
